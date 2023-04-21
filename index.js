6dc7ea1e737c3b01a3f7565a6be1b769693e8ab3
Rostyslav Klym <rostik.klym@gmail.com>
2023-04-21 21:05:32 +0300

    http service fixed

--- index.js
+++ index.js
@@ -1,9 +1,108 @@
-import {getAccessToCabinet, dataVerification} from './getAccessFunction.js';
-import {createVisitCard, createDefaultValue} from './modalWindowfunction.js';
+import { getAccessToCabinet, dataVerification } from "./getAccessFunction.js";
+import { createVisitCard, createDefaultValue } from "./modalWindowfunction.js";
 
-document.querySelector('.btn-enter').addEventListener('click', getAccessToCabinet);
-document.querySelector('.access-btn').addEventListener('click', dataVerification);
-document.querySelector('.choose-doctor').addEventListener('change', createVisitCard);
-document.querySelectorAll('.btn-close--modal').forEach(btn => {
-    btn.addEventListener('click', createDefaultValue);
+class HttpService {
+  constructor() {
+    this.URL = "https://ajax.test-danit.com/api/v2/cards";
+  }
+
+  async signIn(email, password) {
+    return await (
+      await fetch(`${this.URL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email, password: password }),
+      })
+    ).text();
+  }
+
+  async getAllCards(token) {
+    return await (
+      await fetch(this.URL, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    ).json();
+  }
+
+  async getCard(token, id) {
+    return await (
+      await fetch(`${this.URL}/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    ).json();
+  }
+  async postCard(token, cardData) {
+    const { title, description, doctor, bp, age, weight } = cardData;
+    return await (
+      await fetch(this.URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          title: title,
+          description: description,
+          doctor: doctor,
+          bp: bp,
+          age: age,
+          weight: weight,
+        }),
+      })
+    ).json();
+  }
+
+  async updateCard(token, id, cardData) {
+    const { title, description, doctor, bp, age, weight } = cardData;
+    return await (
+      await fetch(`${this.URL}/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          id: id,
+          title: title,
+          description: description,
+          doctor: doctor,
+          bp: bp,
+          age: age,
+          weight: weight,
+        }),
+      })
+    ).json();
+  }
+
+  async deleteCard(token, id) {
+    return await fetch(`${this.URL}/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+}
+
+document
+  .querySelector(".btn-enter")
+  .addEventListener("click", getAccessToCabinet);
+document
+  .querySelector(".access-btn")
+  .addEventListener("click", dataVerification);
+document
+  .querySelector(".choose-doctor")
+  .addEventListener("change", createVisitCard);
+document.querySelectorAll(".btn-close--modal").forEach((btn) => {
+  btn.addEventListener("click", createDefaultValue);
 });

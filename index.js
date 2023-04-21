e48671eb10902a51a6a78fd40fcc4dae6fb41f4b
Max Netus <maxnetus@gmail.com>
2023-04-21 16:00:06 +0300

    sign In and modal window are created

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-import {getAccessToCabinet, dataVerification} from './getAccessFunction.js'
+import {getAccessToCabinet, dataVerification} from './getAccessFunction.js';
+import {createVisitCard, createDefaultValue} from './modalWindowfunction.js';
 
 document.querySelector('.btn-enter').addEventListener('click', getAccessToCabinet);
 document.querySelector('.access-btn').addEventListener('click', dataVerification);
+document.querySelector('.choose-doctor').addEventListener('change', createVisitCard);
+document.querySelectorAll('.btn-close--modal').forEach(btn => {
+    btn.addEventListener('click', createDefaultValue);
+});
 
 
 
 
-class Modal {
-    constructor() {
-    }
-}
 
 
 
-const choseDoctor = document.querySelector('.choose-doctor');
-
-choseDoctor.addEventListener('change', e => {
-    if (choseDoctor.value === 'cardiologist') {
-
-    } else if (choseDoctor.value === 'dentist') {
-
-    } else {
-
-    }
-})

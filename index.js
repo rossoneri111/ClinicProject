ba13da61f37a4be5c3a2bc406be92833a8b5d03a
Rostyslav Klym <rostik.klym@gmail.com>
2023-04-23 18:36:43 +0300

    added CreateVisitModal class

--- index.js
+++ index.js
@@ -97,20 +97,22 @@ class HttpService {
 class ClinicApp {
   constructor() {
     this.handlers = new Handlers();
-    this.addListeners();
     this.token = this.getToken();
     this.render();
+    this.addListeners();
   }
 
   addListeners() {
     const signInForm = document.querySelector("#signInForm");
-    signInForm.addEventListener("submit", this.handlers.signInHandler);
+    if (signInForm)
+      signInForm.addEventListener("submit", this.handlers.signInHandler);
 
-    const createVisitForm = document.querySelector("#createVisitForm");
-    createVisitForm.addEventListener(
-      "submit",
-      this.handlers.createVisitHandler
-    );
+    const createVisitBtn = document.querySelector('[name="createVisitBtn"]');
+    if (createVisitBtn)
+      createVisitBtn.addEventListener(
+        "click",
+        this.handlers.createVisitHandler
+      );
   }
 
   getToken() {
@@ -190,13 +192,12 @@ class Handlers {
     }
   }
 
-  async createVisitHandler(event) {
-    event.preventDefault();
-
-    console.log(new FormData(this));
+  createVisitHandler(event) {
+    const createVisit = new CreateVisitModal();
   }
 }
 
+// Not used
 class Modal {
   constructor() {}
 
@@ -220,6 +221,7 @@ class Modal {
   // }
 }
 
+// Not used
 class SignInModal extends Modal {
   constructor() {
     super();
@@ -271,13 +273,138 @@ class SignInModal extends Modal {
   // }
 }
 
+class CreateVisitModal extends Modal {
+  constructor() {
+    super();
+    this.chooseDoctor = document.querySelector(".choose-doctor");
+    this.addListeners();
+  }
+
+  addListeners() {
+    this.chooseDoctor.addEventListener(
+      "change",
+      this.createVisitCard.bind(this)
+    );
+
+    document.querySelectorAll(".btn-close--modal").forEach((btn) => {
+      btn.addEventListener("click", this.createDefaultValue.bind(this));
+    });
+  }
+
+  createVisitCard() {
+    this.clearForm();
+    if (this.chooseDoctor.value === "cardiologist") {
+      document.querySelector(".cardiologist-option").classList.remove("d-none");
+      this.activateCreateCardBtn();
+    } else if (this.chooseDoctor.value === "dentist") {
+      document.querySelector(".dentist-option").classList.remove("d-none");
+      this.activateCreateCardBtn();
+    } else if (this.chooseDoctor.value === "therapist") {
+      document.querySelector(".therapist-option").classList.remove("d-none");
+      this.activateCreateCardBtn();
+    } else {
+      this.clearForm();
+    }
+  }
+
+  createDefaultValue() {
+    this.chooseDoctor.value = "choose-option";
+    this.clearForm();
+  }
+
+  clearForm() {
+    document.querySelectorAll(".choose-specialist").forEach((specialist) => {
+      specialist.classList.add("d-none");
+    });
+    document.querySelectorAll(".modal-form__data").forEach((data) => {
+      data.value = "";
+    });
+    document.querySelectorAll(".choose-urgency").forEach((data) => {
+      data.value = "choose-urgency";
+    });
+    document.querySelector(".btn-create-visit__card").classList.add("d-none");
+  }
+
+  activateCreateCardBtn() {
+    const cardiologist = document.querySelector(".cardiologist-option");
+    const dentist = document.querySelector(".dentist-option");
+    const therapist = document.querySelector(".therapist-option");
+
+    if (!cardiologist.classList.contains("d-none")) {
+      const cardiologistInputs = document.querySelectorAll(
+        ".cardiologist-option__value"
+      );
+      const cardiologistSelect = document.querySelector(
+        ".cardiologist-option__select"
+      );
+      if (
+        [...cardiologistInputs].every((input) => input.value !== "") &&
+        cardiologistSelect.value !== ""
+      ) {
+        document
+          .querySelector(".btn-create-visit__card")
+          .classList.remove("d-none");
+      } else {
+        cardiologistInputs.forEach((e) => {
+          e.addEventListener("keyup", this.activateCreateCardBtn);
+        });
+        cardiologistSelect.addEventListener(
+          "change",
+          this.activateCreateCardBtn
+        );
+        document
+          .querySelector(".btn-create-visit__card")
+          .classList.add("d-none");
+      }
+    }
+
+    if (!dentist.classList.contains("d-none")) {
+      const dentistInputs = document.querySelectorAll(".dentist-option__value");
+      const dentistSelect = document.querySelector(".dentist-option__select");
+      if (
+        [...dentistInputs].every((input) => input.value !== "") &&
+        dentistSelect.value !== ""
+      ) {
+        document
+          .querySelector(".btn-create-visit__card")
+          .classList.remove("d-none");
+      } else {
+        dentistInputs.forEach((e) => {
+          e.addEventListener("keyup", this.activateCreateCardBtn);
+        });
+        dentistSelect.addEventListener("change", this.activateCreateCardBtn);
+        document
+          .querySelector(".btn-create-visit__card")
+          .classList.add("d-none");
+      }
+    }
+
+    if (!therapist.classList.contains("d-none")) {
+      const therapistInputs = document.querySelectorAll(
+        ".therapist-option__value"
+      );
+      const therapistSelect = document.querySelector(
+        ".therapist-option__select"
+      );
+      if (
+        [...therapistInputs].every((input) => input.value !== "") &&
+        therapistSelect.value !== ""
+      ) {
+        document
+          .querySelector(".btn-create-visit__card")
+          .classList.remove("d-none");
+      } else {
+        therapistInputs.forEach((e) => {
+          e.addEventListener("keyup", this.activateCreateCardBtn);
+        });
+        therapistSelect.addEventListener("change", this.activateCreateCardBtn);
+        document
+          .querySelector(".btn-create-visit__card")
+          .classList.add("d-none");
+      }
+    }
+  }
+}
+
 const httpService = new HttpService();
 const clinicApp = new ClinicApp(httpService);
-console.log(clinicApp);
-
-document
-  .querySelector(".choose-doctor")
-  .addEventListener("change", createVisitCard);
-document.querySelectorAll(".btn-close--modal").forEach((btn) => {
-  btn.addEventListener("click", createDefaultValue);
-});
